c35ac8e3f462fd935e5f82c7db3d7b6c1419d7ed
Benjamin KABEYA **Afrik Interim HR Services ** <benjamin.kabeya@afrikinterim.local>
2024-08-08 18:05:30 +0100

    deployed successfully!

---
+++ start-http-server.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
